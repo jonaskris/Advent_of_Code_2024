92e6e630805c9711aec584dd3d2b1d491e4b1e8d
jonaskris <jo_nas@live.no>
2024-12-03 20:05:38 +0100

    Day 03

--- README.md
+++ README.md
@@ -18,8 +18,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `106.3µs` | `136.3µs` |
-| [Day 2](./src/bin/02.rs) | `289.2µs` | `655.0µs` |
+| [Day 1](./src/bin/01.rs) | `107.0µs` | `131.1µs` |
+| [Day 2](./src/bin/02.rs) | `291.2µs` | `616.1µs` |
+| [Day 3](./src/bin/03.rs) | `641.9µs` | `156.6ms` |
 
-**Total: 1.19ms**
+**Total: 158.39ms**
 <!--- benchmarking table --->
---
+++ src/bin/03.rs
@@ -0,0 +1,90 @@
+advent_of_code::solution!(3);
+
+use itertools::Either;
+use regex::Regex;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let multiplication_re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let multiplications: Vec<(u32, u32)> = multiplication_re
+        .captures_iter(input)
+        .filter_map(|cap| {
+            Some((
+                cap.get(1)?.as_str().parse::<u32>().ok()?,
+                cap.get(2)?.as_str().parse::<u32>().ok()?,
+            ))
+        })
+        .collect();
+
+    let answer = multiplications
+        .iter()
+        .map(|(a, b)| a * b)
+        .sum();
+
+    Some(answer)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let command_re = Regex::new(
+        r"(?<multiplication>mul\((\d+),(\d+)\))|(?<dokeyword>do\(\))|(?<dontkeyword>don't\(\))",
+    )
+        .unwrap();
+
+    let commands: Vec<Either<(u32, u32), &str>> = command_re
+        .captures_iter(input)
+        .filter_map(|cap| {
+            if let Some(multiplication) = cap.name("multiplication") {
+                let nums: Option<(u32, u32)> = Regex::new(r"mul\((\d+),(\d+)\)")
+                    .unwrap()
+                    .captures(multiplication.as_str())
+                    .and_then(|cap| {
+                        Some((
+                            cap.get(1)?.as_str().parse::<u32>().ok()?,
+                            cap.get(2)?.as_str().parse::<u32>().ok()?,
+                        ))
+                    });
+
+                nums.map(Either::Left)
+            } else if let Some(dokeyword) = cap.name("dokeyword") {
+                Some(Either::Right(dokeyword.as_str()))
+            } else if let Some(dontkeyword) = cap.name("dontkeyword") {
+                Some(Either::Right(dontkeyword.as_str()))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mut currently_dont = false;
+    let answer = commands.iter().fold(0, |acc, command| match command {
+        Either::Left((a, b)) if !currently_dont => acc + (a * b),
+        Either::Right("do()") => {
+            currently_dont = false;
+            acc
+        }
+        Either::Right("don't()") => {
+            currently_dont = true;
+            acc
+        }
+        _ => acc,
+    });
+
+    Some(answer)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(161));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(48));
+    }
+}
